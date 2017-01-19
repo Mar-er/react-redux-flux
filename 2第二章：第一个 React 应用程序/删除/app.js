9f96f91e8572b08bc8b083386daf7c9471a336be
mar-er <1051221329@qq.com>
2017-01-19 20:44:17 +0800

    第一次提交代码

---
+++ 2第二章：第一个 React 应用程序/删除/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+      ReactDOM.render(
+      	<div>	
+			<button className="btn btn-default">Add</button>
+			<ul className="list-group">
+				<li className="list-group-item">
+					fds
+					<a href="javascript:;" className="right glyphicon glyphicon-edit"></a>
+					<a href="javascript:;" className="right glyphicon glyphicon-remove"></a>
+				</li>
+				<li className="list-group-item">
+					<input type="text" className="edit" />
+					<a href="javascript:;" className="glyphicon glyphicon-share"></a>
+					<a href="javascript:;" className="glyphicon glyphicon-remove"></a>
+				</li>
+			</ul>
+      	</div>,
+        document.getElementById('container')
+      );
